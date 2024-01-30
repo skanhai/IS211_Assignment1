d24f6db0d895b048f6a26d7eac93efb05fd9827e
Steven Kanhai <steven.kanha@gmail.com>
2024-01-29 21:16:47 -0500

    IS211_Assignment1 part 1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,36 @@
 
-def list_divide(numbers, divide):
+def list_divide(numbers, divide=2):
     """
     The function returns the number of elements in the numbers list that are divisibleby divide
     """
+    counter = 0
+    for num in numbers:
+        if num % divide == 0:
+            counter += 1
+    return counter
+
+
+
+class ListDivideException(Exception):
     pass
+
+
 
 def test_list_divide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+
+    try:
+        assert list_divide([1, 2, 3, 4, 5]) == 2
+        assert list_divide([2, 4, 6, 8, 10]) == 5
+        assert list_divide([30, 54, 63, 98, 100], divide=10) == 2
+        assert list_divide([]) == 0
+        assert list_divide([1, 2, 3, 4, 5], 1) == 5
+    except AssertionError:
+        raise ListDivideException("One or more tests have failed")
+
+
+
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()

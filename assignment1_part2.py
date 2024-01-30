eceaf7cbbc2e96a66e117b64fdfb8e0ba58b7892
Steven Kanhai <steven.kanha@gmail.com>
2024-01-29 21:16:59 -0500

    IS211_Assignment1 part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,27 @@
 
 class Book:
-    def __init__(self, author, title):
-        pass
+    def __init__(self, title="", author=""):
+
+        """
+        Constructor of Book class
+
+        :param author: The author of the book
+        :param title:  The title of the book
+         """
+        self.author = author
+        self.title = title
+
     
     def display(self):
-        pass
+
+        print(f" {self.title}, written by {self.author}. ")
 
 
 if __name__ == "__main__":
-    pass
+
+    b1 = Book("Harry Potter and the Goblet of Fire", "J. K. Rowling")
+    b2 = Book("Ivanhoe: A Romance", "Walter Scott")
+
+
+    b1.display()
+    b2.display()
